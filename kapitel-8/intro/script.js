07d0353452e97e307a95e0a72429bdc8387bb799
Calin Alexuta <calin.alexuta@elev.ga.ntig.se>
2025-03-13 11:06:54 +0100

    tränat på variabler och konkatenering

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,17 @@
+// var ålder = 17;
+// var tal1 = 12;
+// var tal2 = 24;
+// var summa = tal1 + tal2;
+// alert(TAL1 + " + " + tal2 + " = " + summa);
+// alert(ålder);
+
+var förnamn = "Calin";
+var efternamn = "Georgescu";
+var helaNamnet = förnamn + " " + efternamn;
+var hobby = "racing"
+var kostnad = 23465789809
+
+// Tex: Calins hobby är racing
+// Min hobby kostar
+var mening = helaNamnet + "'s hobby är" + " " + hobby + ".\nMin hobby kostar" + " " + kostnad + " " + "kr/min"; 
+alert(mening);

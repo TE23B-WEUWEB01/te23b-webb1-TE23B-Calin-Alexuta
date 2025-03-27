69065fa1a51339c4620f7f80f18d929531977b2f
Calin Alexuta <calin.alexuta@elev.ga.ntig.se>
2025-03-27 11:08:16 +0100

    tränat på onclick, funktioner, querySelector()

---
+++ kapitel-8/interaktivitet/script.js
@@ -0,0 +1,22 @@
+function cioara() {
+    document.body.style = "background: #000; color: #FFF;";
+    document.querySelector("h1").style = "font-size: 100px"
+}
+
+function taran() {
+    document.body.style = "background: #FFF; color: #000;";
+}
+
+function salamalejkum() {
+    document.body.style = "background: #246; color: #972;";
+    document.querySelector("h1").style = "font-size: 100px"
+}
+
+function låt() {
+    document.querySelector("p").style = "visibility: visible;";
+}
+
+function dölj() {
+    document.querySelector("p").style = "visibility: hidden;";
+}
+
